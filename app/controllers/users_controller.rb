8008a6489e62e65e0bd2c33cac6ddaebd54ce024
Jeff Sutherland <jdsuther@asu.edu>
2016-06-23 14:57:56 -0700

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
