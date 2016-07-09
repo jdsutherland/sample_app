5f0f20848a22f3160521f4293b86e51dcf663696
Jeff Sutherland <jdsuther@asu.edu>
2016-07-09 13:57:33 -0700

    Add Microposts access control tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,15 +48,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
