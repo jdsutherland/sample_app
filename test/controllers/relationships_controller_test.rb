51eb02fc6b953469db9b6fc71bb8ae587d6aa2b9
Jeff Sutherland <jdsuther@asu.edu>
2016-07-14 01:24:10 -0700

    Fix relationships integration test

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RelationshipsControllerTest < ActionController::TestCase
+class RelationshipsControllerTest < ActionDispatch::IntegrationTest
 
   test "create should require logged-in user" do
     assert_no_difference 'Relationship.count' do
@@ -11,9 +11,8 @@ class RelationshipsControllerTest < ActionController::TestCase
 
   test "destroy should require logged-in user" do
     assert_no_difference 'Relationship.count' do
-      delete relationships_path(relationships(:one))
+      delete relationship_path(relationships(:one))
     end
     assert_redirected_to login_url
   end
-
 end
